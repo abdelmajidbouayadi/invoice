81fba03c0d395b5585f8634d23f712c473873f52
Abdelmajid <bouajudfa@gmail.com>
2022-01-23 21:03:39 +0100

    fix nav bar

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -6,9 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
-
+  affiche = {
+    inventory: false,
+    sales: false,
+    purchases: false
+  }
   constructor() { }
-
+  onClick(word: string){
+    if(word === 'inventory') this.affiche.inventory = !this.affiche.inventory;
+    else if(word === 'sales') this.affiche.sales = !this.affiche.sales;
+    else this.affiche.purchases = !this.affiche.purchases;
+  }
   ngOnInit(): void {
   }
 
